b0ef308b43503934787f06862d4bd7bb9769fc60
pikaqiu <1047984367@qq.com>
2019-01-03 22:11:07 +0800

    代码初始化

--- src/main/java/com/pikaqiu/design/principle/singleresponsibility/Test.java
+++ src/main/java/com/pikaqiu/design/principle/singleresponsibility/Test.java
@@ -1,7 +1,8 @@
 package com.pikaqiu.design.principle.singleresponsibility;
 
 /**
- * Created by geely
+ * 类的单一职责原则  和 方法的单一职责类似
+ * 为了保证解耦，和维护成本低 一个类或者接口最好就一个职责
  */
 public class Test {
     public static void main(String[] args) {
