82526c92e606126fded79b3114a4ecea1c8ba234
pikaqiu <1047984367@qq.com>
2019-01-03 23:22:33 +0800

    代码初始化

--- src/main/java/com/pikaqiu/design/principle/interfacesegregation/Dog.java
+++ src/main/java/com/pikaqiu/design/principle/interfacesegregation/Dog.java
@@ -1,9 +1,14 @@
 package com.pikaqiu.design.principle.interfacesegregation;
 
 /**
- * Created by geely
+ * 接口原则一定要适度
+ * 太细了实现麻烦
+ * 太大了不能拆分成具体实现
+ * <p>
+ * 接口隔离侧重 对接口依赖的隔离
+ * 单一职责 侧重于职责（功能）
  */
-public class Dog implements ISwimAnimalAction,IEatAnimalAction {
+public class Dog implements ISwimAnimalAction, IEatAnimalAction {
 
     @Override
     public void eat() {
