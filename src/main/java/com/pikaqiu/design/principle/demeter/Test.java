4fbe2540fe6a0957a3ce3cee0b650e5a65181e39
1047984367@qq.com <as1314520>
2019-01-04 10:24:43 +0800

    增加注释

--- src/main/java/com/pikaqiu/design/principle/demeter/Test.java
+++ src/main/java/com/pikaqiu/design/principle/demeter/Test.java
@@ -1,7 +1,9 @@
 package com.pikaqiu.design.principle.demeter;
 
 /**
- * Created by geely
+ * 最少知道原则 成员变量算强依赖
+ * 类内部高内聚 类外部低耦合
+ * 少和其他类产生依赖
  */
 public class Test {
     public static void main(String[] args) {
