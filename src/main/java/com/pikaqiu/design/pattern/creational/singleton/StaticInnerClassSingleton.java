81e455cd9b5db60ca915d321a1d75e5bbf092757
pikaqiu <1047984367@qq.com>
2019-01-10 00:06:17 +0800

    代码初始化

--- src/main/java/com/pikaqiu/design/pattern/creational/singleton/StaticInnerClassSingleton.java
+++ src/main/java/com/pikaqiu/design/pattern/creational/singleton/StaticInnerClassSingleton.java
@@ -1,15 +1,29 @@
 package com.pikaqiu.design.pattern.creational.singleton;
 
 /**
- * Created by geely
+ * 静态内部类方式
+ * 延迟初始化
  */
 public class StaticInnerClassSingleton {
+
+    /**
+     * 静态内部类 内初始化 单例实例
+     */
     private static class InnerClass{
         private static StaticInnerClassSingleton staticInnerClassSingleton = new StaticInnerClassSingleton();
     }
+
+    /**
+     * 静态方法被调用 满足初始化条件
+     * @return
+     */
     public static StaticInnerClassSingleton getInstance(){
         return InnerClass.staticInnerClassSingleton;
     }
+
+    /**
+     * 私有构造器 防止被new
+     */
     private StaticInnerClassSingleton(){
         if(InnerClass.staticInnerClassSingleton != null){
             throw new RuntimeException("单例构造器禁止反射调用");
