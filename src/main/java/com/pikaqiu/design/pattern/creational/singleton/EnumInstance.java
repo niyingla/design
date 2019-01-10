aa016676be7292d72c8a1a0eeb22cebb74f6f2c8
1047984367@qq.com <as1314520>
2019-01-10 21:06:05 +0800

    增加注释

--- src/main/java/com/pikaqiu/design/pattern/creational/singleton/EnumInstance.java
+++ src/main/java/com/pikaqiu/design/pattern/creational/singleton/EnumInstance.java
@@ -5,6 +5,7 @@ package com.pikaqiu.design.pattern.creational.singleton;
  */
 public enum EnumInstance {
     INSTANCE{
+        @Override
         protected  void printTest(){
             System.out.println("Geely Print Test");
         }
