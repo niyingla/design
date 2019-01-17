284b2eef493e6cc545cc2bf23d3a654df8e4577d
pikaqiu <1047984367@qq.com>
2019-01-17 22:54:50 +0800

    代码初始化

--- src/main/java/com/pikaqiu/design/pattern/structural/facade/PointsGift.java
+++ src/main/java/com/pikaqiu/design/pattern/structural/facade/PointsGift.java
@@ -1,6 +1,7 @@
 package com.pikaqiu.design.pattern.structural.facade;
 
 /**
+ * 积分礼物
  * @author xiaoye
  */
 public class PointsGift {
--- src/main/java/com/pikaqiu/design/pattern/structural/facade/QualifyService.java
+++ src/main/java/com/pikaqiu/design/pattern/structural/facade/QualifyService.java
@@ -1,9 +1,16 @@
 package com.pikaqiu.design.pattern.structural.facade;
 
 /**
+ * 子系统
  * @author xiaoye
  */
 public class QualifyService {
+
+    /**
+     *  是否有资格
+     * @param pointsGift
+     * @return
+     */
     public boolean isAvailable(PointsGift pointsGift){
         System.out.println("校验"+pointsGift.getName()+" 积分资格通过,库存通过");
         return true;
