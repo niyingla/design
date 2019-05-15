3fa747f8652c3e6d71f0a4863e422a2e1537f340
1047984367@qq.com <as1314520>
2019-05-15 11:42:54 +0800

    修改注释

--- src/main/java/com/pikaqiu/design/principle/interfacesegregation/Bird.java
+++ src/main/java/com/pikaqiu/design/principle/interfacesegregation/Bird.java
@@ -1,6 +1,9 @@
 package com.pikaqiu.design.principle.interfacesegregation;
 
 /**
+ * 接口隔离原则
+ *   客户端不应该依赖它不需要的接口；
+ *   一个类对另一个类的依赖应该建立在最小的接口上。
  * @author xiaoye
  */
 public class Bird implements IAnimalAction {
