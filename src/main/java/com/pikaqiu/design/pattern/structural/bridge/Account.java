b1e92fdd29ad98fe878385e99e052a6a6c150383
1047984367@qq.com <as1314520>
2019-10-16 16:51:18 +0800

    增加注释

--- src/main/java/com/pikaqiu/design/pattern/structural/bridge/Account.java
+++ src/main/java/com/pikaqiu/design/pattern/structural/bridge/Account.java
@@ -4,7 +4,15 @@ package com.pikaqiu.design.pattern.structural.bridge;
  * @author xiaoye
  */
 public interface Account {
+    /**
+     * 开户
+     * @return
+     */
     Account openAccount();
+
+    /**
+     * 展示账号类型
+     */
     void showAccountType();
 
 }
--- src/main/java/com/pikaqiu/design/pattern/structural/bridge/Bank.java
+++ src/main/java/com/pikaqiu/design/pattern/structural/bridge/Bank.java
@@ -5,9 +5,18 @@ package com.pikaqiu.design.pattern.structural.bridge;
  */
 public abstract class Bank {
     protected Account account;
+
+    /**
+     * @param account
+     */
     public Bank(Account account){
         this.account = account;
     }
+
+    /**
+     * 打开账户
+     * @return
+     */
     abstract Account openAccount();
 
 }
