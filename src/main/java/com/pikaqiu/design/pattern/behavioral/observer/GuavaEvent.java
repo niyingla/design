01c57c3311208ac21dd28e3bed77f847244a686f
huohua <ye.xiao@qiyucloud.com.cn>
2021-07-28 15:50:13 +0800

    判断规则执行器

--- src/main/java/com/pikaqiu/design/pattern/behavioral/observer/GuavaEvent.java
+++ src/main/java/com/pikaqiu/design/pattern/behavioral/observer/GuavaEvent.java
@@ -1,6 +1,7 @@
 package com.pikaqiu.design.pattern.behavioral.observer;
 
 import com.google.common.eventbus.Subscribe;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Random;
 
@@ -10,6 +11,7 @@ import java.util.Random;
  * @author: xiaoye
  * @create: 2019-01-28 00:01
  **/
+@Slf4j
 public class GuavaEvent {
 
     /**
@@ -18,10 +20,10 @@ public class GuavaEvent {
      * @param str
      */
     @Subscribe
-    public void postEvent(String str){
+    public void postEvent(String str) {
         if (new Random().nextBoolean()) {
             throw new RuntimeException("报错啦");
         }
-        System.out.println("观察者方法被调用内容是：" + str);
+        log.info("观察者方法被调用内容是：" + str);
     }
 }
--- src/main/java/com/pikaqiu/design/pattern/behavioral/observer/GuavaEventTest.java
+++ src/main/java/com/pikaqiu/design/pattern/behavioral/observer/GuavaEventTest.java
@@ -2,6 +2,7 @@ package com.pikaqiu.design.pattern.behavioral.observer;
 
 
 import com.google.common.eventbus.EventBus;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @program: design_pattern
@@ -9,6 +10,7 @@ import com.google.common.eventbus.EventBus;
  * @author: xiaoye
  * @create: 2019-01-27 23:59
  **/
+@Slf4j
 public class GuavaEventTest {
 
     public static void main(String[] args) {
@@ -18,15 +20,15 @@ public class GuavaEventTest {
         GuavaEvent guavaEvent = new GuavaEvent();
         GuavaEvent1 guavaEvent1 = new GuavaEvent1();
 
-        System.out.println("开始注册事件");
+        log.info("开始注册事件");
         //注册需要观察的对象的方法
         eventBus.register(guavaEvent);
         eventBus.register(guavaEvent1);
-        System.out.println("注册事件完毕");
+        log.info("注册事件完毕");
         //调用触发观察 上报事件
-        System.out.println("开始上报事件");
+        log.info("开始上报事件");
         eventBus.post("post调用啊");
-        System.out.println("上报完毕事件");
+        log.info("上报完毕事件");
     }
 
 }
